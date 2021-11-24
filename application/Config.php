66c0c3dc0308f8f54b0ad0eefdc733b41682d304
Segundo <segundogaldames@gmail.com>
2021-11-24 15:13:40 -0300

    Archivos principales del core de la app

---
+++ application/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASE_URL', 'http://localhost:8080/proyecto-cib402/');
+define('DEFAULT_CONTROLLER', 'index');
+define('DEFAULT_LAYOUT', 'default');
+
+define('SESSION_TIME', 10);
+define('HASH_KEY', '6i806265k789c618025a9a1300');
+define('CTRL', '6380h4hg563c096189e25b490de6');
